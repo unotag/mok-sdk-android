f70f7b6998831bad4275fcf484179bb5045afcfa
sohelyadav <sohelyadav@gmail.com>
2023-10-03 09:39:41 +0530

    refactor : push notification permission

--- app/src/main/java/one/mok/MainActivity.kt
+++ app/src/main/java/one/mok/MainActivity.kt
@@ -1,108 +1,28 @@
 package one.mok
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
-import com.unotag.mokone.utils.MokLogger
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
-import com.unotag.mokone.MokSDK
-import com.unotag.mokone.pushNotification.fcm.PushNotificationPermissionHandler
-import one.mok.databinding.ActivityMainBinding
-import org.json.JSONObject
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var pushNotificationPermissionHandler: PushNotificationPermissionHandler
-
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_main)
 
-        getFcmToken()
-        pushNotificationPermissionHandler = PushNotificationPermissionHandler(
-            applicationContext, this
-        )
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager = findViewById(R.id.viewPager)
 
-        binding.notificationPermissionBtn.setOnClickListener {
-            pushNotificationPermissionHandler.requestPermission()
-        }
+        val adapter = HomeViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(DashboardFragment(), "Dashboard")
+        adapter.addFragment(SettingsFragment(), "Settings")
 
-        binding.openNotificationSettingsBtn.setOnClickListener {
-            pushNotificationPermissionHandler.openNotificationSettings()
-        }
-
-        binding.fcmTokenValue.setOnClickListener {
-            copyTextToClipboard(binding.fcmTokenValue)
-        }
-    }
-
-    private fun copyTextToClipboard(textToCopy: TextView) {
-        val text = textToCopy.text.toString()
-        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clipData = ClipData.newPlainText("Copied Text", text)
-        clipboardManager.setPrimaryClip(clipData)
-        showToast("Text copied to clipboard")
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        setNotificationPermissionStatus()
-    }
-
-
-    private fun setNotificationPermissionStatus() {
-        var isGranted = pushNotificationPermissionHandler.isNotificationPermissionGranted()
-        binding.notificationSettingsStatus.text =
-            if (isGranted) "Granted" else "Denied"
-
-        var textColor =
-            if (isGranted) resources.getColor(R.color.colorSuccess) else resources.getColor(R.color.colorError)
-
-        binding.notificationSettingsStatus.setTextColor(textColor)
-    }
-
-    private fun getFcmToken() {
-        val mokSDK = MokSDK.getInstance(applicationContext)
-        mokSDK.getFCMToken { token, error ->
-            if (token != null) {
-                binding.fcmTokenValue.text = token
-
-                // mokSDK.triggerWorkflow("63185f8b-5390-4091-a4ef-a31fab165dae", jsonBody)
-                //-   updateUser(token)
-            } else {
-                binding.fcmTokenValue.text = error
-
-            }
-        }
-    }
-
-    private fun updateUser(fcmToken: String?) {
-        val mokSDK = MokSDK.getInstance(applicationContext)
-        val jsonBody = JSONObject()
-        jsonBody.put("name", "rra_test_user")
-        jsonBody.put("fcm_registration_token", fcmToken ?: "N/A")
-        mokSDK.updateUser("TEST_RR_KABLE_001", jsonBody) { success, errorMessage ->
-            if (success != null) {
-                // API call was successful, do something
-                MokLogger.log(MokLogger.LogLevel.DEBUG, "update user result : $success")
-            } else {
-                // API call failed, handle the error
-                if (errorMessage != null) {
-                    // Display or log the error message
-                    MokLogger.log(MokLogger.LogLevel.ERROR, "update user result : $errorMessage")
-                }
-            }
-        }
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
     }
 }
